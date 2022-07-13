4f35bae7694a0a43d6542d295e6df6cd346ff143
Randy Flores Mateo <randy.rrfm@gmail.com>
2022-07-13 16:18:56 -0700

    Initial Commit with simple Call to API.
    Home page created with a navigation bar to future features of the app. Homepage has a simple Input Text and Button that makes a call to "The Movie Database" api.

--- app/src/main/java/com/ruggery/streaming_where/MainActivity.java
+++ app/src/main/java/com/ruggery/streaming_where/MainActivity.java
@@ -3,12 +3,60 @@ package com.ruggery.streaming_where;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
+    private final OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText et_ShowName = (EditText) findViewById(R.id.et_ShowName);
+        Button searchShow = (Button) findViewById(R.id.searchShow);
+
+        searchShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {                        //api key: 38b4e8be946536c96c2ebffa3cbd988d
+                retrieveID();
+            }
+        });
+    }
+
+    public void retrieveID(){
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/search/movie?api_key=38b4e8be946536c96c2ebffa3cbd988d&query=Jack+Reacher")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    ResponseBody responseBody = response.body();
+                    Log.d("demo","onResponse: " + responseBody.string());
+                }
+            }
+        });
     }
 }
